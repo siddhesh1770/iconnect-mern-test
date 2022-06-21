73e5a866433c95f78e4314827e526ae39ce78ee3
Siddhesh Patil <hello@siddheshpatil.in>
2022-06-22 03:43:05 +0530

    material ui setup

--- frontend/src/components/TempTable.js
+++ frontend/src/components/TempTable.js
@@ -1,85 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import {getAllCompanies} from '../service/api'
-import { Table } from 'react-bootstrap';
-import { MDBDataTable } from 'mdbreact';
+import React from 'react'
+import { DataGrid} from '@mui/x-data-grid';
+import testData from './testData.json';
+import { useNavigate } from 'react-router-dom';
 
 const TempTable = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        getAllData();
-    }, [])
-    const getAllData = async () => {
-        let res = await getAllCompanies();
-        const columns = [
-            {
-                label: 'Company Name',
-                field: 'name',
-                sort: 'asc',
-                width: 150
-            },
-            {
-                label: 'Phone',
-                field: 'phone',
-                sort: 'asc',
-                width: 100
-            },
-            {
-                label: 'Email',
-                field: 'email',
-                sort: 'asc',
-                width: 100
-            },
-            {
-                label: 'State',
-                field: 'state',
-                sort: 'asc',
-                width: 100
-            },
-            {
-                label: 'City',
-                field: 'city',
-                sort: 'asc',
-                width: 100
-            },
-            {
-                label: 'Description',
-                field: 'description',
-                sort: 'asc',
-                width: 100
-            }
-        ]
-        const rows = [];
-        res.data.forEach(element => {
-            rows.push({
-                name: element.name,
-                phone: element.phone,
-                email: element.email,
-                state: element.state,
-                city: element.city,
-                description: element.description
-            })
-        })
-        setData({ columns, rows });
+    const updateInfo = async (data) => {
+        console.log(data)
     }
-    console.log(data)
+    const inputHandler = (e, id) => {
+        console.log(e.target.value);
+        console.log(id);
+    }
+    const cols = [
+        {
+            headerName: 'Serial',
+            field: 'serial',
+            width: 100,
+            sortable: true,
+        },
+        {
+            headerName: 'Name',
+            field: 'name',
+            width: 150,
+            filter: true,
+            editable: true,
+        },
+        {
+            headerName: 'Email',
+            field: 'email',
+            width: 200,
+            editable: true,
+        },
+        {
+            headerName: 'Phone',
+            field: 'phone',
+            width: 150,
+            editable: true,
+        },
+        {
+            headerName: 'State',
+            field: 'state',
+            width: 180,
+            // filter: true,
+            editable: true,
+        },
+        {
+            headerName: 'City',
+            field: 'city',
+            width: 150,
+            editable: true,
+            // filter: true,
+        },
+        {
+            headerName: 'Descrpiption',
+            field: 'description',
+            width: 750,
+            editable: true,
+            id: 232323,
+            // add funcionality to edit and save in database
+
+        }
+    ]
+    // convert all serial to id from testData.data
+    let data = testData.data;
+    data.forEach(element => {
+        element.mongoId = element.id;
+        element.id = element.serial;
+    });
+    const hello = () => {
+        console.log('hello');
+    }
   return (
-    <>
-    <div>
-        <br></br>
-        <h1>
-            All Companies Listed
-        </h1>
+    <div style={{height: 400, width: '100%'}} className="main-table">
+        <DataGrid 
+            rows={data}
+            columns={cols}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            onCellEditCommit={(e) => {
+                console.log('onCellEditCommit');
+                //search for id in data
+                data.forEach(element => {
+                    if(element.id === e.id){
+                        console.log(element);
+                    }
+                })
+            }}
+        />
     </div>
-    <div className="main-table">
-    <MDBDataTable
-      striped
-      bordered
-      small
-      dark
-      data={data}
-    />
-    </div>
-    </>
   )
 }
 
